3f0e282809675110973369f1362e7f6b20e9f6bf
devmac2012 <devmac2012@devmac2012.local>
2018-12-21 12:20:50 +0900

    データの表示

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,4 +1,15 @@
 class BlogsController < ApplicationController
+  before_action :set_blog, only: :show
+
   def index
+    @blogs = Blog.all.order(:name)
+  end
+
+  def show
+  end
+
+  private
+  def set_blog
+    @blog = Blog.find(params[:id])
   end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,13 @@
 class PostsController < ApplicationController
+  before_action :set_post
   def index
   end
 
   def show
+  end
+
+  private
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
