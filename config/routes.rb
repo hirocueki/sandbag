7703f22a8abef62dfe1e0a20b71605f9ff6ac86c
Hiroshi Ueki <hirocueki@gmail.com>
2018-12-23 21:25:11 +0900

    add user show index

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+
   root "blogs#index"
   resources :blogs do
     resources :posts do
       resources :comments
     end
   end
+
+  resources :users, only: %i[show index]
 end
